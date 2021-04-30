5638195a88874ad6cb7161a611fe17f5826fac4a
Ivanir Efi <efi.ivanir@gmail.com>
2021-04-30 17:04:06 +0300

    Ch 03

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from config import Config
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+app.config.from_object(Config)
 
 bootstrap = Bootstrap()
 bootstrap.init_app(app)
+
 from app import routes
